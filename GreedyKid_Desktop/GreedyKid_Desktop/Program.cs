49c4f390c1f2700b5201da5354567fe8fc01404f
Thomas Altenburger <mister.helmut@gmail.com>
2017-01-17 10:11:47 +0100

    Initial project setup

---
+++ GreedyKid_Desktop/GreedyKid_Desktop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GreedyKid_Desktop
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GreedyKidGame())
+                game.Run();
+        }
+    }
+}
